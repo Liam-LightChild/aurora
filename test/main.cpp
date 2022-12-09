23298af5cd77f6d00393744c960e97ba8da62e06
liam <liam_lightchild@outlook.com>
2022-12-08 23:21:56 -0800

    transform test to use textures

--- test/main.cpp
+++ test/main.cpp
@@ -1,50 +1,58 @@
 #include "aurora/application.h"
-#include "aurora/resources/shader.h"
-#include "aurora/resources/buffer.h"
-#include "aurora/resources/draw_object.h"
+#include "aurora/resources.h"
 
 class MainApplication : public aurora::Application {
 private:
 	aurora::Shader *m_Shader;
 	aurora::Buffer *m_VertexBuffer, *m_IndexBuffer;
 	aurora::DrawObject *m_DrawObject;
+	aurora::Texture2D *m_Texture;
 	aurora::VertexArrangement m_TestShaderArrange = {
 		{"v_position", aurora::VertexInputType::Float, 3},
-		{"v_color", aurora::VertexInputType::Float, 3}
+		{"v_color", aurora::VertexInputType::Float, 3},
+		{"v_texCoords", aurora::VertexInputType::Float, 2}
 	};
 
 public:
 	MainApplication() : m_Shader(getInstance()->getAssetLoader()->load<aurora::Shader>("test:test.shader")),
 	                    m_VertexBuffer(new aurora::Buffer(aurora::VertexBuffer)),
-						m_IndexBuffer(new aurora::Buffer(aurora::IndexBuffer)) {
+						m_IndexBuffer(new aurora::Buffer(aurora::IndexBuffer)),
+						m_Texture(getInstance()->getAssetLoader()->load<aurora::Texture2D>("test:test.texture")){
 		float vertexBuf[] = {
-			0, 0, 0, 1, 0, 0,
-			0, 1, 0, 0, 1, 0,
-			1, 0, 0, 0, 0, 1,
+			0, 0, 0, 1, 0, 0, 0, 1,
+			0, 1, 0, 0, 1, 0, 0, 0,
+			1, 0, 0, 0, 0, 1, 1, 1,
+			1, 1, 0, 1, 1, 1, 1, 0,
 		};
 
 		uint32_t indexBuf[] = {
-			0, 1, 2
+			0, 1, 2,
+			1, 3, 2
 		};
 
 		m_VertexBuffer->update(vertexBuf, sizeof(vertexBuf));
 		m_IndexBuffer->update(indexBuf, sizeof(indexBuf));
 
-		m_DrawObject = new aurora::DrawObject(aurora::DrawObjectOptions {
-			.shader = m_Shader->getReference(),
-			.vertexBuffer = m_VertexBuffer->getReference(),
-			.indexBuffer = m_IndexBuffer->getReference(),
-			.vertexCount = 3,
-			.indexBufferItemType = aurora::IndexBufferItemType::UnsignedInt,
-			.arrangement = m_TestShaderArrange
-		});
+		aurora::DrawObjectOptions options;
+		options.shader = m_Shader->getReference();
+		options.vertexBuffer = m_VertexBuffer->getReference();
+		options.indexBuffer = m_IndexBuffer->getReference();
+		options.vertexCount = 6;
+		options.indexBufferItemType = aurora::IndexBufferItemType::UnsignedInt;
+		options.arrangement = m_TestShaderArrange;
+		options.textures[0] = m_Texture->getReference();
+
+		m_DrawObject = new aurora::DrawObject(options);
 	}
 
 	~MainApplication() override {
 		delete m_DrawObject;
 		delete m_VertexBuffer;
 		delete m_IndexBuffer;
-		getInstance()->getAssetLoader()->unload<aurora::Shader>(m_Shader);
+
+		auto a = getInstance()->getAssetLoader();
+		a->unload<aurora::Shader>(m_Shader);
+		a->unload<aurora::Texture2D>(m_Texture);
 	}
 
 	void render() override {

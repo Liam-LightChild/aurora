83ec617010c1bfe530bf8ac458dfbcbbd7ee4a5b
liam <liam_lightchild@outlook.com>
2022-12-13 19:22:21 -0800

    Add --info to ameshc

--- tools/ameshc/ameshc.cpp
+++ tools/ameshc/ameshc.cpp
@@ -4,6 +4,48 @@
 #include <fstream>
 #include <aurora/aether/aether.h>
 
+const char *info = R"(
+--- Information ---------------------------------------------------------------
+
+ameshc: Compiles Wavefront OBJ meshes files into a binary mesh format to be loaded
+by an Aurora application.
+
+This tool is part of the Aurora Game Engine. Use --help to view the help message.
+
+--- License -------------------------------------------------------------------
+
+BSD 3-Clause License
+
+Copyright (c) 2022, der_frühling
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+-------------------------------------------------------------------------------
+)";
+
 namespace po = boost::program_options;
 
 void split2(const std::string &pString, std::string &p1, std::string &p2, char pChar = ' ') {
@@ -29,6 +71,7 @@ int main(int pArgCount, char** pArgs) {
 
 	desc.add_options()
 		    ("help", "Produce help message")
+		    ("info", "Produce information message")
 		    ("output-file,o", po::value<std::string>()->required(), "Destination path")
 		    ("input-file,i", po::value<std::string>()->required(), "Provide Input file")
 		    ("mesh-path,m", po::value<std::string>()->required(), "Provide mesh path");
@@ -36,6 +79,11 @@ int main(int pArgCount, char** pArgs) {
 	po::variables_map vm;
 	po::store(po::command_line_parser(pArgCount, pArgs).options(desc).positional(pd).run(), vm);
 	po::notify(vm);
+
+	if(vm.count("info")) {
+		std::cout << info << std::endl;
+		return 0;
+	}
 
 	if(vm.count("help") || !vm.count("input-file") || !vm.count("output-file") || !vm.count("mesh-path")) {
 		std::cerr << desc << std::endl;
